6848310dba75301216834b4589cba30a963beb77
mlinhou <mlinhou@gmail.com>
2024-02-22 15:14:44 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Item from './components/item';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TODO LIST</h1>
+      <Item/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/item.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './item.css'
+
+export default function item() {
+  return (
+    <div className='item-container'>item</div>
+  )
+}
