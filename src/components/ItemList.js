36c1b6fa29d1c2ff4fdfa2fef09820f9e0955b7c
mlinhou <mlinhou@gmail.com>
2024-02-24 15:10:03 -0600

    add new notes

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,8 +4,9 @@ import Modal from 'react-modal';
 
 function ItemList() {
 
-  const itemList = [];
+  const [itemList, setItemList] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
+  const [textInput, setTextInput] = useState('');
 
   const openModal = () => {
     setIsOpen(true);
@@ -15,19 +16,45 @@ function ItemList() {
     setIsOpen(false);
   };
 
+  const handleChange = (e) => {
+    setTextInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    itemList.push(textInput);
+    setItemList(itemList);
+    setTextInput('');
+    closeModal();
+  };
+
+  const showList = () => {
+    console.log(itemList);
+  }
   return (
     <div>
-      <Item note='Hello'/>
+      <ul>
+        {itemList.map((item) => (
+          <Item note={item}/>
+        ))
+        }
+      </ul>
       <button onClick={openModal}>Open Modal</button>
       <Modal
         isOpen={isOpen}
         onRequestClose={closeModal}
-        contentLabel="Example Modal"
       >
         <h2>NOTE TITLE</h2>
-        <p>Note content here...</p>
-        <button onClick={closeModal}>Close</button> {/* Close button inside modal */}
+        <input
+            type="text"
+            value={textInput}
+            onChange={handleChange}
+            placeholder="Enter text..."
+          />
+        <button onClick={handleSubmit}>Save Note</button> {/* Close button inside modal */}
+        
       </Modal>
+      <button onClick={showList}>Show list</button>
     </div>
 
   )
