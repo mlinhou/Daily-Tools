7ac947c63c1e762fbc1fd27872269aa866c6356f
mlinhou <mlinhou@gmail.com>
2024-02-24 14:38:30 -0600

    add modal popup

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Item from './Item'
+import Modal from 'react-modal';
 
 function ItemList() {
+
+  const itemList = [];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Item note='Hello'/>
+    <div>
+      <Item note='Hello'/>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <h2>NOTE TITLE</h2>
+        <p>Note content here...</p>
+        <button onClick={closeModal}>Close</button> {/* Close button inside modal */}
+      </Modal>
+    </div>
+
   )
 }
 
