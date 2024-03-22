7da5347dd28d556d6716325dd87f046e67c0f7b3
mlinhou <mlinhou@gmail.com>
2024-03-22 17:31:25 -0500

    add sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import * as FaIcons from "react-icons/fa"
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import './Sidebar.css';
+import { IconContext } from 'react-icons';
+
+function Sidebar() {
+    const [sidebar, setSideBar] = useState(false)
+
+    const showSideBar = () => setSideBar(!sidebar)
+
+  return (
+    
+    <div className='nav'>
+        <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='sidebar'>
+            <Link to="#" className='menu-bars'>
+                <FaIcons.FaBars onClick={showSideBar}/>
+            </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+            <ul className='nav-menu-items' onClick={showSideBar}>
+                <li className='sidebar-toggle'>
+                    <Link to="#" className='menu-bars'>
+                        <AiIcons.AiOutlineClose />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Home">
+                        <button className="home">Home</button>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Todo">
+                        <button className="todo">Todo</button>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Calculator">
+                        <button className="calc">Calculator</button>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Timer">
+                        <button className="time">Timer</button>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+        </IconContext.Provider>
+    </div>
+
+  )
+}
+
+export default Sidebar
