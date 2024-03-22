07f259f5cb62dc726b41237b93307afff986ccb7
mlinhou <mlinhou@gmail.com>
2024-03-22 11:05:50 -0500

    add calculator css

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import "./Calculator.css";
 
 function Calculator() {
   return (
-    <div>Calculator</div>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">123,234</div>
+        <div className='current-operand'>123,234</div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+
+    </div>
   )
 }
 
